3ab3fc6378b49b24410e3a8fedf2dc38a5df48cc
Eliezer1109 <eliezerdasilva503@gmail.com>
2024-10-23 20:07:24 -0300

    🎉  iniciando aulas de APi

--- 07_10/index.js
+++ 07_10/index.js
@@ -1,8 +1,22 @@
+const caixas = document.querySelector(".caixas");
 
 const createCards = () => {
-    let data =[];
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((Response) =>{
-        data = Response.data;
+
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) =>{
+        const info = response?.data;
+
+        for( const card of info){
+            caixas.innerHTML += `
+            <div class="caixa">
+                <h2>${card.title}</h2>
+                <p>${card.body}</p>
+            </div>
+        `
+        }
+
+        
+
+        console.log(response)
     }).catch((error) => {
         console.log("deu um erro cabuloso",error)
         console.error(error);
